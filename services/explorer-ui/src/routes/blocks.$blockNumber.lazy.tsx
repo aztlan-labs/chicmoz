92495c9a05178a69f908b003bab4f161466f7cc0
filip <filip.harald@gmail.com>
2024-09-10 14:45:52 +0200

    extracted to hook

--- services/explorer-ui/src/routes/blocks.$blockNumber.lazy.tsx
+++ services/explorer-ui/src/routes/blocks.$blockNumber.lazy.tsx
@@ -1,10 +1,12 @@
 import { createLazyFileRoute } from "@tanstack/react-router";
+import { useLatestBlock } from "~/hooks/useLatestBlock";
 
 export const Route = createLazyFileRoute("/blocks/$blockNumber")({
   component: Block,
 });
 
 function Block() {
+  const { latestBlockData, loading, error, timeSince } = useLatestBlock();
   const { blockNumber } = Route.useParams();
   console.log("blockNumber", blockNumber);
 
@@ -16,8 +18,35 @@ function Block() {
     <div className="bg-card p-4">
       {bn ? (
         <div>
-          <h2>Block "{bn}"</h2>
-          <p>Block details here</p>
+          <h2 className="text-2xl font-bold mb-4">Latest Block Data</h2>
+          {loading && <p>Loading...</p>}
+          {error && <p className="text-red-500">{error}</p>}
+          {latestBlockData && (
+            <ul className="space-y-2">
+              <li>
+                <strong>Block Number:</strong>{" "}
+                {parseInt(
+                  latestBlockData.header.globalVariables.blockNumber,
+                  16
+                )}
+              </li>
+              <li>
+                <strong>Time since:</strong> {timeSince}
+              </li>
+              <li>
+                <strong>Number of Transactions:</strong>{" "}
+                {parseInt(latestBlockData.header.contentCommitment.numTxs, 16)}
+              </li>
+              <li>
+                <strong>Total Fees:</strong>{" "}
+                {parseInt(latestBlockData.header.totalFees, 16)} (wei)
+              </li>
+              <li>
+                <strong>State Root:</strong>{" "}
+                {latestBlockData.header.state.partial.publicDataTree.root}
+              </li>
+            </ul>
+          )}
         </div>
       ) : (
         <div>
--- services/explorer-ui/src/routes/index.lazy.tsx
+++ services/explorer-ui/src/routes/index.lazy.tsx
@@ -1,69 +1,8 @@
-import { ChicmozL2Block } from "@chicmoz-pkg/types";
 import { createLazyFileRoute } from "@tanstack/react-router";
-import { useCallback, useEffect, useState } from "react";
-import { getLatestBlock } from "~/service/api";
-
-const formatTimeSince = (seconds: number) => {
-  const intervals = [
-    { label: "day", seconds: 86400 },
-    { label: "hour", seconds: 3600 },
-    { label: "minute", seconds: 60 },
-    { label: "second", seconds: 1 },
-  ];
-
-  for (let i = 0; i < intervals.length; i++) {
-    const interval = intervals[i];
-    const count = Math.floor(seconds / interval.seconds);
-    if (count >= 1) {
-      return `${count} ${interval.label}${count > 1 ? "s" : ""} ago`;
-    }
-  }
-  return "just now";
-};
+import { useLatestBlock } from "~/hooks/useLatestBlock";
 
 const LatestBlockData = () => {
-  const [latestBlockData, setLatestBlockData] = useState<ChicmozL2Block | null>(
-    null
-  );
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState<string | null>(null);
-
-  const fetchLatestBlock = useCallback(async () => {
-    try {
-      const block = await getLatestBlock();
-      console.log("block", block);
-      if (
-        !block ||
-        latestBlockData?.header?.globalVariables?.blockNumber ===
-          block?.header?.globalVariables?.blockNumber
-      )
-        return;
-
-      setLatestBlockData(block);
-      setError(null);
-    } catch (err) {
-      console.error("Error fetching latest block:", err);
-      setError("Failed to fetch latest block. Please try again later.");
-      setLatestBlockData(null);
-    } finally {
-      setLoading(false);
-    }
-  }, [latestBlockData?.header?.globalVariables?.blockNumber]);
-
-  useEffect(() => {
-    fetchLatestBlock(); // Fetch immediately on mount
-    const intervalId = setInterval(fetchLatestBlock, 3000); // Poll every 3 seconds
-    return () => clearInterval(intervalId); // Clean up on unmount
-  }, [fetchLatestBlock]);
-
-  let timeSince = "no timestamp";
-  if (latestBlockData) {
-    const now = new Date().getTime();
-    const blockTime = new Date(
-      parseInt(latestBlockData.header.globalVariables.timestamp, 16) * 1000
-    ).getTime();
-    timeSince = formatTimeSince(Math.round((now - blockTime) / 1000));
-  }
+  const { latestBlockData, loading, error, timeSince } = useLatestBlock();
 
   return (
     <div className="bg-gray-100 p-6 rounded-lg shadow-md mt-8">
