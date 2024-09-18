ebba4c3c406f3ee0959b62e8d8e2d34c27aeed66
filip <filip.harald@gmail.com>
2024-09-18 21:27:55 +0200

    less spam locally

--- services/aztec-listener/src/aztec/catchup.ts
+++ services/aztec-listener/src/aztec/catchup.ts
@@ -1,3 +1,4 @@
+import { NODE_ENV } from "../constants.js";
 import { onBlock } from "../event-handler/index.js";
 import { logger } from "../logger.js";
 import { getBlock } from "./network-client.js";
@@ -11,10 +12,13 @@ export const startCatchup = async ({
 }) => {
   for (let i = from; i < to; i++) {
     const blockRes = await getBlock(i);
-    if (!blockRes) 
-      throw new Error("FATAL: Catchup received no block.");
-    
+    if (!blockRes) throw new Error("FATAL: Catchup received no block.");
+
     logger.info(`🐨 publishing block ${i} (of total ${to - from + 1})...`);
     await onBlock(blockRes);
+    if (NODE_ENV === "development") {
+      // NOTE: if we are restarting locally often we shouln't spam their RPC
+      await new Promise((resolve) => setTimeout(resolve, 500));
+    }
   }
 };
