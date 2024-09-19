a0af332a3a556a235d54578900ec89dcce847b42
Filip Harald <Filip.harald@gmail.com>
2024-09-19 15:23:54 +0200

    Update services/explorer-api/src/database/controllers/utils.ts

--- services/explorer-api/src/database/controllers/utils.ts
+++ services/explorer-api/src/database/controllers/utils.ts
@@ -4,7 +4,7 @@ import { logger } from "../../logger.js";
 export const dbParseErrorCallback = (e: Error) => {
   if (e instanceof ZodError) {
     logger.error(`FATAL - dbParseErrorCallback: ${JSON.stringify(e.issues)}`);
-    // NOTE: throwing normal error so we give 500 status code
+    // NOTE: throwing normal error so we give 500 status code in http-server
     throw new Error("Internal server error (DB)");
   } else {
     // NOTE: this should never happen
