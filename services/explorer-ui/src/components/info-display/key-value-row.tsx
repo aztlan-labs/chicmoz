0d2064fb239dd7d8203f9967adb313727dfeb0f0
Mauro <mauro@eijsenring.com>
2024-09-19 16:31:35 +0200

    update info display to suppport clickable links

--- services/explorer-ui/src/components/info-display/key-value-row.tsx
+++ services/explorer-ui/src/components/info-display/key-value-row.tsx
@@ -3,16 +3,29 @@ import { FC } from "react";
 interface KeyValueRowProps {
   label: string;
   value: string;
+  isClickable?: boolean;
   isLast?: boolean;
 }
 
-export const KeyValueRow: FC<KeyValueRowProps> = ({ label, value, isLast }) => (
+export const KeyValueRow: FC<KeyValueRowProps> = ({
+  label,
+  value,
+  isLast,
+  isClickable = false,
+}) => (
   <div
     className={`flex items-center py-3 ${
       !isLast ? "border-b border-gray-200" : ""
     }`}
   >
     <span className="text-gray-600 w-1/5">{label}</span>
-    <span className="font-medium text-right">{value}</span>
+    <span
+      className={`text-sm flex-grow ${
+        isClickable ? "text-blue-600 cursor-pointer" : ""
+      }`}
+    >
+      {value}
+      {isClickable && <span className="ml-1">🔗</span>}
+    </span>
   </div>
 );
