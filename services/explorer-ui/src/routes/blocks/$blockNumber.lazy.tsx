7b0a2414fe0098910c05ab5b5ef2cf1b13c9e5b5
Mauro <mauro@eijsenring.com>
2024-09-19 02:25:00 +0200

    update block route

--- services/explorer-ui/src/routes/blocks/$blockNumber.lazy.tsx
+++ services/explorer-ui/src/routes/blocks/$blockNumber.lazy.tsx
@@ -1,13 +1,17 @@
 import { createLazyFileRoute } from "@tanstack/react-router";
+import { TransactionsTable } from "~/components/transactions/transactions-table";
+import { Button } from "~/components/ui";
 import { useLatestBlock } from "~/hooks/useLatestBlock";
 
 export const Route = createLazyFileRoute("/blocks/$blockNumber")({
   component: Block,
 });
+
 function Block() {
-  const { latestBlockData, loading, error, timeSince } = useLatestBlock();
+  const { mockedChicmozL2Block, loading, error, timeSince } = useLatestBlock();
   const { blockNumber } = Route.useParams();
 
+  const latestBlockData = mockedChicmozL2Block;
   // TODO: these messages should perhaps be diplayed?
   // console.log("Extracted messages:");
   // const txEffects = latestBlockData?.body.txEffects;
@@ -41,7 +45,7 @@ function Block() {
     const displayItems = items.slice(0, maxItems);
     return (
       <ul className="list-disc list-inside pl-4">
-      {/* eslint-disable-next-line @typescript-eslint/no-explicit-any */}
+        {/* eslint-disable-next-line @typescript-eslint/no-explicit-any */}
         {displayItems.map((item: any, index: number) => (
           <li key={index}>
             {typeof item === "object" ? (
@@ -61,112 +65,35 @@ function Block() {
   };
 
   return (
-    <div className="bg-card p-4">
+    <div className="mx-auto px-[70px] max-w-[1440px]">
       {bn ? (
-        <div>
-          <h2 className="text-2xl font-bold mb-4">Block Data</h2>
-          {loading && <p>Loading...</p>}
-          {error && <p className="text-red-500">{error}</p>}
-          {latestBlockData && (
-            <>
-              <div className="mb-4">
-                <p>
-                  <strong>Block Number:</strong>{" "}
-                  {parseInt(
-                    latestBlockData.header.globalVariables.blockNumber,
-                    16
-                  )}
-                </p>
-                <p>
-                  <strong>Time since:</strong> {timeSince}
-                </p>
-              </div>
-              <h3 className="text-xl font-semibold mb-2">
-                Transaction Effects
-              </h3>
-              {latestBlockData.body.txEffects.map((txEffect, index) => (
-                <details
-                  key={index}
-                  className="mb-4 p-2 border border-gray-200 rounded"
-                >
-                  <summary className="font-semibold cursor-pointer">
-                    Transaction {index + 1}
-                  </summary>
-                  <div className="mt-2">
-                    <ul className="space-y-1">
-                      <li>
-                        <strong>Transaction Fee:</strong>{" "}
-                        {parseInt(txEffect.transactionFee, 16)} (wei)
-                      </li>
-                      <li>
-                        <details>
-                          <summary>
-                            <strong>Note Hashes:</strong>{" "}
-                            {txEffect.noteHashes.length}
-                          </summary>
-                          {renderList(txEffect.noteHashes)}
-                        </details>
-                      </li>
-                      <li>
-                        <details>
-                          <summary>
-                            <strong>Nullifiers:</strong>{" "}
-                            {txEffect.nullifiers.length}
-                          </summary>
-                          {renderList(txEffect.nullifiers)}
-                        </details>
-                      </li>
-                      <li>
-                        <details>
-                          <summary>
-                            <strong>L2 to L1 Messages:</strong>{" "}
-                            {txEffect.l2ToL1Msgs.length}
-                          </summary>
-                          {renderList(txEffect.l2ToL1Msgs)}
-                        </details>
-                      </li>
-                      <li>
-                        <details>
-                          <summary>
-                            <strong>Public Data Writes:</strong>{" "}
-                            {txEffect.publicDataWrites.length}
-                          </summary>
-                          {renderList(txEffect.publicDataWrites)}
-                        </details>
-                      </li>
-                      <li>
-                        <details>
-                          <summary>
-                            <strong>Note Encrypted Logs:</strong>{" "}
-                            {txEffect.noteEncryptedLogs.functionLogs.length}
-                          </summary>
-                          {renderList(txEffect.noteEncryptedLogs.functionLogs)}
-                        </details>
-                      </li>
-                      <li>
-                        <details>
-                          <summary>
-                            <strong>Encrypted Logs:</strong>{" "}
-                            {txEffect.encryptedLogs.functionLogs.length}
-                          </summary>
-                          {renderList(txEffect.encryptedLogs.functionLogs)}
-                        </details>
-                      </li>
-                      <li>
-                        <details>
-                          <summary>
-                            <strong>Unencrypted Logs:</strong>{" "}
-                            {txEffect.unencryptedLogs.functionLogs.length}
-                          </summary>
-                          {renderList(txEffect.unencryptedLogs.functionLogs)}
-                        </details>
-                      </li>
-                    </ul>
-                  </div>
-                </details>
-              ))}
-            </>
-          )}
+        <div className="">
+          <div className="bg-white p-6 rounded-lg shadow-md mt-8 mb-8">
+            <h2 className="text-2xl font-bold mb-4">Block Data</h2>
+            {loading && <p>Loading...</p>}
+            {error && <p className="text-red-500">{error}</p>}
+            {latestBlockData && (
+              <>
+                <div>
+                  <p>
+                    <strong>Block Number:</strong>{" "}
+                    {parseInt(
+                      latestBlockData.header.globalVariables.blockNumber,
+                      16,
+                    )}
+                  </p>
+                  <p>
+                    <strong>Time since:</strong> {timeSince}
+                  </p>
+                </div>
+              </>
+            )}
+          </div>
+          <div className="flex flex-row gap-4 w-10 mb-4">
+            <Button variant={"primary"}>View Transactions</Button>
+            <Button variant={"primary"}>View Transactions</Button>
+          </div>
+          <TransactionsTable />
         </div>
       ) : (
         <div>
