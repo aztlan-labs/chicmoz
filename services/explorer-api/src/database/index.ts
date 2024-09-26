17a954445dd1437ef64ceea8a067a687087785ff
Filip Harald <Filip.harald@gmail.com>
2024-09-26 15:00:50 +0200

    feat: better error-messages for DB in explorer-api (#59)

--- services/explorer-api/src/database/index.ts
+++ services/explorer-api/src/database/index.ts
@@ -4,6 +4,8 @@ import { dbCredentials } from "../environment.js";
 import * as schema from "./schema/index.js";
 
 let db: ReturnType<typeof drizzle>;
+let initialized = false;
+let shutDown = false;
 
 export const init = async () => {
   const client = new pg.Client(dbCredentials);
@@ -12,12 +14,22 @@ export const init = async () => {
 
   await client.connect();
 
+  initialized = true;
+
   return {
     shutdownDb: async () => {
+      shutDown = true;
       await client.end();
     },
   };
 };
 
-export const getDb = () => db;
+export const getDb = () => {
+  if (!initialized) throw new Error("Database not initialized");
+
+  if (shutDown) throw new Error("Database has been shut down");
+
+  return db;
+};
+
 export * as controllers from "./controllers/index.js";
