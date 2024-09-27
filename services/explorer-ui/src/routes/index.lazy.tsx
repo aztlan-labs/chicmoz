8dea1cb5757035d94aff1a33d8dcf0f28f806f9c
Mauro <mauro@eijsenring.com>
2024-09-27 17:59:39 +0200

    latest block uses new hook

--- services/explorer-ui/src/routes/index.lazy.tsx
+++ services/explorer-ui/src/routes/index.lazy.tsx
@@ -1,38 +1,32 @@
 import { createLazyFileRoute } from "@tanstack/react-router";
-import { useLatestBlock } from "~/hooks/useLatestBlock";
+import { useLatestBlock } from "~/hooks/";
 
 const LatestBlockData = () => {
-  const { latestBlockData, loading, error, timeSince } = useLatestBlock();
+  const { data: latestBlock, isLoading, error } = useLatestBlock();
+
+  if (isLoading) return <p>Loading...</p>;
+  if (error) return <p className="text-red-500">{error.message}</p>;
+  if (!latestBlock) return <p>No data</p>;
 
   return (
-    <div className="bg-gray-100 p-6 rounded-lg shadow-md mt-8">
-      <h2 className="text-2xl font-bold mb-4">Latest Block Data</h2>
-      {loading && <p>Loading...</p>}
-      {error && <p className="text-red-500">{error}</p>}
-      {latestBlockData && (
-        <ul className="space-y-2">
-          <li>
-            <strong>Block Number:</strong>{" "}
-            {parseInt(latestBlockData.header.globalVariables.blockNumber, 16)}
-          </li>
-          <li>
-            <strong>Time since:</strong> {timeSince}
-          </li>
-          <li>
-            <strong>Number of Transactions:</strong>{" "}
-            {parseInt(latestBlockData.header.contentCommitment.numTxs, 16)}
-          </li>
-          <li>
-            <strong>Total Fees:</strong>{" "}
-            {parseInt(latestBlockData.header.totalFees, 16)} (wei)
-          </li>
-          <li>
-            <strong>State Root:</strong>{" "}
-            {latestBlockData.header.state.partial.publicDataTree.root}
-          </li>
-        </ul>
-      )}
-    </div>
+    <ul className="space-y-2">
+      <li>
+        <strong>Block Number:</strong>{" "}
+        {parseInt(latestBlock.header.globalVariables.blockNumber, 16)}
+      </li>
+      <li>
+        <strong>Number of Transactions:</strong>{" "}
+        {parseInt(latestBlock.header.contentCommitment.numTxs, 16)}
+      </li>
+      <li>
+        <strong>Total Fees:</strong>{" "}
+        {parseInt(latestBlock.header.totalFees, 16)} (wei)
+      </li>
+      <li>
+        <strong>State Root:</strong>{" "}
+        {latestBlock.header.state.partial.publicDataTree.root}
+      </li>
+    </ul>
   );
 };
 
@@ -44,7 +38,10 @@ function Index() {
   return (
     <div className="mx-auto px-[70px] max-w-[1440px]">
       <h1 className="text-center mt-16">{text.exploreThePrivacyOnAztec}</h1>
-      <LatestBlockData />
+      <div className="bg-gray-100 p-6 rounded-lg shadow-md mt-8">
+        <h2 className="text-2xl font-bold mb-4">Latest Block Data</h2>
+        <LatestBlockData />
+      </div>
     </div>
   );
 }
