0f1ff0a1e05ac3765b701ef685498e7b0a23bb14
Filip Harald <Filip.harald@gmail.com>
2024-09-30 16:38:37 +0200

    feat: add hook for tx-effects (#64)

--- services/explorer-ui/src/api/tx-effect.ts
+++ services/explorer-ui/src/api/tx-effect.ts
@@ -1,6 +1,10 @@
+import {
+  chicmozL2TxEffectSchema,
+  type ChicmozL2TxEffect,
+} from "@chicmoz-pkg/types";
 import { z } from "zod";
-import client, { validateResponse } from "./client";
 import { aztecExplorer } from "~/service/constants";
+import client, { validateResponse } from "./client";
 
 export const TxEffectsAPI = {
   getTxEffectById: async (txId: string): Promise<number> => {
@@ -11,24 +15,26 @@ export const TxEffectsAPI = {
     });
     return validateResponse(z.number(), response.data);
   },
-  getTxEffectsByHeight: async (height: number): Promise<number> => {
+  getTxEffectsByHeight: async (
+    height: number
+  ): Promise<ChicmozL2TxEffect[]> => {
     const response = await client.get(
-      aztecExplorer.getL2TxEffectsByHeight(height),
+      aztecExplorer.getL2TxEffectsByHeight(height)
     );
-    return validateResponse(z.number(), response.data);
+    return validateResponse(z.array(chicmozL2TxEffectSchema), response.data);
   },
   getTxEffectByHeightAndIndex: async (
     height: number,
-    index: number,
+    index: number
   ): Promise<number> => {
     const response = await client.get(
-      aztecExplorer.getL2TxEffectByHeightAndIndex(height, index),
+      aztecExplorer.getL2TxEffectByHeightAndIndex(height, index)
     );
     return validateResponse(z.number(), response.data);
   },
   getTxEffectsByHeightRange: async (
     start: number,
-    end: number,
+    end: number
   ): Promise<number> => {
     const response = await client.get(
       aztecExplorer.getL2TxEffectsByHeightRange,
@@ -37,7 +43,7 @@ export const TxEffectsAPI = {
           start,
           end,
         },
-      },
+      }
     );
     return validateResponse(z.number(), response.data);
   },
