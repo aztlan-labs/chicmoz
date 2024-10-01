2fe200eb66c38ab1db1efd6000324d6a258d398d
Mauro <mauro@eijsenring.com>
2024-10-01 15:46:14 +0200

    edit the txeffect page

--- services/explorer-ui/src/routes/transactions/$transactionId.lazy.tsx
+++ services/explorer-ui/src/routes/transactions/$transactionId.lazy.tsx
@@ -1,32 +1,14 @@
+import { ChicmozL2TxEffect } from "@chicmoz-pkg/types";
 import { createLazyFileRoute } from "@tanstack/react-router";
 import { useState } from "react";
 import { KeyValueDisplay } from "~/components/info-display/key-value-display";
+import { TxEffextDetailsDisplay } from "~/components/tx-effect-details-display";
 import { Button } from "~/components/ui";
+import { useGetTxEffectById } from "~/hooks/tx-effect";
 
 export const Route = createLazyFileRoute("/transactions/$transactionId")({
   component: TransactionDetails,
 });
-
-const transactionData = [
-  {
-    label: "HASH",
-    value: "0xb950b8ba84a4f35f85d5f62a0e3f8b161d8733479c14d426dc36ae30f7e96246",
-    isClickable: true,
-  },
-  { label: "STATUS", value: "SUCCESS" },
-  {
-    label: "BLOCK NUMBER",
-    value: "348540 (confirmed by sequencer)",
-    isClickable: true,
-  },
-  { label: "TIMESTAMP", value: "1 min ago (Jun-07-2024 08:47:23 AM UTC)" },
-  {
-    label: "L1 BATCH TX",
-    value: "0xb950b8ba84a4f35f85d5f62a0e3f8b161d8733479c14d426dc36ae30f7e92341",
-    isClickable: true,
-  },
-  { label: "TRANSACTION FEE", value: "0.000012245344159887766 ETH ($0.05)" },
-];
 
 const feeData = [
   { label: "TRANSACTION FEE", value: "0.000012453 ETH" },
@@ -47,59 +29,80 @@ const feeData = [
 function TransactionDetails() {
   const [selectedButton, setSelectedButton] = useState("transactionEffects");
   const { transactionId } = Route.useParams();
+  const {
+    data: txEffects,
+    isLoading,
+    error,
+  } = useGetTxEffectById(transactionId);
+
+  const getTxEffectData = (data: ChicmozL2TxEffect) => [
+    {
+      label: "HASH",
+      value: data.txHash,
+      isClickable: true,
+    },
+    { label: "STATUS", value: "SUCCESS" },
+    {
+      label: "TRANSACTION FEE",
+      value: data.transactionFee,
+      isClickable: true,
+    },
+    //TODO: get actual Block number and time stamp
+    { label: "BLOCK NUMBER", value: "-1" },
+    { label: "TIMESTAMP", value: "-1" },
+  ];
+
+  if (!transactionId) <div> No transaction id</div>;
+
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>Error</div>;
+  if (!txEffects) return <div>No data</div>;
 
   return (
     <div className="mx-auto px-[70px] max-w-[1440px]">
-      {transactionId ? (
+      <div>
         <div>
-          <div>
-            <h2>Transaction details</h2>
-            <p>{transactionId}</p>
+          <h2>Transaction details</h2>
+          <p>{txEffects.txHash}</p>
+        </div>
+        <div className="flex flex-col gap-4 mt-8">
+          <div className="bg-white rounded-lg shadow-md p-4">
+            <KeyValueDisplay data={getTxEffectData(txEffects)} />
           </div>
-          <div className="flex flex-col gap-4 mt-8">
+          <div className="flex flex-row gap-4 w-10 mb-4">
+            <Button
+              variant={"primary"}
+              onClick={() => setSelectedButton("transactionEffects")}
+            >
+              Transaction effects
+            </Button>
+            <Button
+              variant={"primary"}
+              onClick={() => setSelectedButton("feeDetails")}
+            >
+              View Transactions
+            </Button>
+          </div>
+          {selectedButton === "transactionEffects" && (
+            <TxEffextDetailsDisplay
+              title="Transaction effects"
+              details={txEffects.effects}
+            />
+          )}
+          {selectedButton === "feeDetails" && (
             <div className="bg-white rounded-lg shadow-md p-4">
-              <KeyValueDisplay data={transactionData} />
-            </div>
-            <div className="flex flex-row gap-4 w-10 mb-4">
-              <Button
-                variant={"primary"}
-                onClick={() => setSelectedButton("transactionEffects")}
-              >
-                Transaction effects
-              </Button>
-              <Button
-                variant={"primary"}
-                onClick={() => setSelectedButton("feeDetails")}
-              >
-                View Transactions
-              </Button>
-            </div>
-            {selectedButton === "transactionEffects" && (
-              <div className="bg-white rounded-lg shadow-md p-4">
-                <h3>Transaction Effects</h3>
+              <div className="flex flex-col mt-8">
+                <h3>Fee Details</h3>
+                <KeyValueDisplay data={feeData} />
               </div>
-            )}
-            {selectedButton === "feeDetails" && (
-              <div className="bg-white rounded-lg shadow-md p-4">
-                <div className="flex flex-col mt-8">
-                  <h3>Fee Details</h3>
-                  <KeyValueDisplay data={feeData} />
-                </div>
-                <div className="flex flex-col mt-8">
-                  <h3>Public fee Details</h3>
-                  <KeyValueDisplay data={feeData} />
-                </div>
+              <div className="flex flex-col mt-8">
+                <h3>Public fee Details</h3>
+                <KeyValueDisplay data={feeData} />
               </div>
-            )}
-          </div>
+            </div>
+          )}
         </div>
-      ) : (
-        //TODO: create reusable component
-        <div>
-          <h2>Invalid Transaction Id</h2>
-          <p>transaction {transactionId} not found</p>
-        </div>
-      )}
+      </div>
     </div>
   );
 }
