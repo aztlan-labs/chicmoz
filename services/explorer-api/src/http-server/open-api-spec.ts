250032a8bd7399a6f5296e38f60dcfe9817aac4d
filip <filip.harald@gmail.com>
2024-10-03 09:32:30 +0200

    minor changes

--- services/explorer-api/src/http-server/open-api-spec.ts
+++ services/explorer-api/src/http-server/open-api-spec.ts
@@ -4,23 +4,23 @@ export const genereateOpenApiSpec = () => ({
   "openapi": "3.1.0",
   "info": {
     "title": "Aztec Scan API",
-    "version": "0.1.0",
-    "description": "API for exploring Aztec Network",
+    "version": "0.2.0",
+    "description": "API for exploring Aztec Network. Please note that this is a work in progress and the API is subject to change.",
   },
   "servers": [
     {
-      "url": "https://api.chicmoz.info/v1/{projectId}",
+      "url": "https://api.chicmoz.info/v1/{apiKey}",
       "variables": {
-        "projectId": {
+        "apiKey": {
           "default": "d1e2083a-660c-4314-a6f2-1d42f4b944f4",
           "description": "The project ID"
         }
       }
     },
     {
-      "url": "http://explorer-api.localhost/v1/{projectId}",
+      "url": "http://explorer-api.localhost/v1/{apiKey}",
       "variables": {
-        "projectId": {
+        "apiKey": {
           "default": "d1e2083a-660c-4314-a6f2-1d42f4b944f4",
           "description": "The project ID"
         }
