2f1a02c9fd14972f7ae823b0167a846e454e88ab
Mauro <mauro@eijsenring.com>
2024-10-03 15:45:50 +0200

    update hooks

---
+++ services/explorer-ui/src/hooks/contract.ts
@@ -0,0 +1,12 @@
+import { ChicmozL2ContractInstanceDeluxe } from "@chicmoz-pkg/types";
+import { type UseQueryResult, useQuery } from "@tanstack/react-query";
+import { ContractL2API } from "~/api";
+
+export const useContractInstance = (
+  address: string,
+): UseQueryResult<ChicmozL2ContractInstanceDeluxe, Error> => {
+  return useQuery<ChicmozL2ContractInstanceDeluxe, Error>({
+    queryKey: ["latestBlock"],
+    queryFn: () => ContractL2API.getContracInstance(address),
+  });
+};
