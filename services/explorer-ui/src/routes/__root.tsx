45f1210c53a4d19e6b297d249faa3ce22bfa078f
filip <filip.harald@gmail.com>
2024-10-03 10:15:11 +0200

    remove

--- services/explorer-ui/src/routes/__root.tsx
+++ services/explorer-ui/src/routes/__root.tsx
@@ -1,8 +1,20 @@
 import { createRootRoute, Outlet } from "@tanstack/react-router";
-import { TanStackRouterDevtools } from "@tanstack/router-devtools";
+import {lazy} from "react";
 import { Footer } from "~/components/footer";
 import { Header } from "~/components/header";
 import { TailwindIndicator } from "~/components/ui/tailwind-indicator";
+
+const TanStackRouterDevtools =
+  process.env.NODE_ENV === 'production'
+    ? () => null // Render nothing in production
+    : lazy(() =>
+        // Lazy load in development
+        import('@tanstack/router-devtools').then((res) => ({
+          default: res.TanStackRouterDevtools,
+          // For Embedded Mode
+          // default: res.TanStackRouterDevtoolsPanel
+        })),
+      )
 
 export const Route = createRootRoute({
   component: () => (
