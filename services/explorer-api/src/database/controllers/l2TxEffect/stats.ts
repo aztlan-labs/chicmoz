d839cda0d5839620a7ef5d5b1af6f8e43d57169e
filip <filip.harald@gmail.com>
2024-10-07 13:50:35 +0200

    24hrs

--- services/explorer-api/src/database/controllers/l2TxEffect/stats.ts
+++ services/explorer-api/src/database/controllers/l2TxEffect/stats.ts
@@ -1,14 +1,38 @@
-import { count } from "drizzle-orm";
+import { and, count, eq, gt, lt } from "drizzle-orm";
 import { getDb as db } from "../../../database/index.js";
-import { txEffect } from "../../../database/schema/index.js";
+import {
+  body,
+  bodyToTxEffects,
+  globalVariables,
+  header,
+  l2Block,
+  txEffect,
+} from "../../../database/schema/l2block/index.js";
 
 export const getTotalTxEffects = async (): Promise<number> => {
   const dbRes = await db().select({ count: count() }).from(txEffect).execute();
   return dbRes[0].count;
 };
 
-// eslint-disable-next-line @typescript-eslint/require-await
+const ONE_DAY = 24 * 60 * 60 * 1000;
 export const getTotalTxEffectsLast24h = async (): Promise<number> => {
-  // TODO: we need l2Block.header.globalVariables.timestamp as number to sum this
-  return -1;
-}
+  const dbRes = await db()
+    .select({ count: count() })
+    .from(txEffect)
+    .innerJoin(bodyToTxEffects, eq(txEffect.id, bodyToTxEffects.txEffectId))
+    .innerJoin(body, eq(body.id, bodyToTxEffects.bodyId))
+    .innerJoin(l2Block, eq(l2Block.bodyId, body.id))
+    .innerJoin(header, eq(header.id, l2Block.headerId))
+    .innerJoin(
+      globalVariables,
+      eq(globalVariables.id, header.globalVariablesId)
+    )
+    .where(
+      and(
+        gt(globalVariables.timestamp, Date.now() - ONE_DAY),
+        lt(globalVariables.timestamp, Date.now())
+      )
+    )
+    .execute();
+  return dbRes[0].count;
+};
