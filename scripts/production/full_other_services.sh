d48e60e7ccafcd588be2c0010df08822c4d65a0f
filip <filip.harald@gmail.com>
2024-10-10 09:43:05 +0200

    fixed one and added scripts

---
+++ scripts/production/full_other_services.sh
@@ -0,0 +1,4 @@
+kubectl scale statefulset --replicas 3 -n chicmoz-prod kafka-controller
+kubectl scale statefulset --replicas 1 -n chicmoz-prod redis-master
+kubectl scale statefulset --replicas 3 -n chicmoz-prod redis-replicas
+kubectl scale statefulset --replicas 1 -n chicmoz-prod postgresql
