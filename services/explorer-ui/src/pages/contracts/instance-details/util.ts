b7380dc6a6c1a20b5694f185979e8264e4c40b4c
filip <filip.harald@gmail.com>
2024-10-15 13:47:44 +0200

    instance link to deployment

--- services/explorer-ui/src/pages/contracts/instance-details/util.ts
+++ services/explorer-ui/src/pages/contracts/instance-details/util.ts
@@ -1,9 +1,15 @@
 import { type ChicmozL2ContractInstanceDeluxe } from "@chicmoz-pkg/types";
+import {routes} from "~/routes/__root";
 
 export const getContractData = (data: ChicmozL2ContractInstanceDeluxe) => [
   {
     label: "ADDRESS",
     value: data.address,
+  },
+  {
+    label: "CLASS ID",
+    value: data.contractClassId,
+    link: `${routes.contracts.route}${routes.contracts.children.classes.route}/${data.contractClassId}`,
   },
   {
     label: "BLOCK HASH",
