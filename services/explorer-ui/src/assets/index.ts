a4a9b6b99de7ac540092e8ba44148258f13ab111
Mauro <mauro@eijsenring.com>
2024-10-16 12:23:19 +0200

    added dropdown on mobile

--- services/explorer-ui/src/assets/index.ts
+++ services/explorer-ui/src/assets/index.ts
@@ -1,6 +1,7 @@
 import AztecIcon from "./aztec-icon.svg?react";
+import AztecIconWhite from "./aztec-icon-white.svg?react";
 import AztecLogoWhite from "./aztec-logo-white.svg?react";
 import AztecLogo from "./aztec-logo.svg?react";
 import BackgroundArt from "./background-art.svg?react";
 
-export { AztecLogo, AztecIcon, AztecLogoWhite, BackgroundArt };
+export { AztecLogo, AztecIcon, AztecLogoWhite, BackgroundArt, AztecIconWhite };
