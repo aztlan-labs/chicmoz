e9b6ca687179613220dd48fbfdbe91e3730086e7
Filip Harald <Filip.harald@gmail.com>
2024-10-17 14:17:56 +0200

    feat: replace "aztec scan" with "chicmoz" and added SEO (#137)

--- services/explorer-ui/src/pages/block-details/index.tsx
+++ services/explorer-ui/src/pages/block-details/index.tsx
@@ -45,10 +45,12 @@ export const BlockDetails: FC = () => {
               <KeyValueDisplay data={getBlockDetails(latestBlock)} />
             </div>
             <div className="flex flex-row gap-4 w-10 mb-4">
-              <Button variant={"default"}>
+              <Button 
+              variant={"default"}
+              className={"shadow-[0px_0px_1px_2px_rgba(0,0,0,1)]"}
+              >
                 <p>View TxEffects</p>
               </Button>
-              <Button variant={"default"}>View TxEffects</Button>
             </div>
             <TxEffectsTable txEffects={getTxEffects(latestBlock)} />
           </div>
