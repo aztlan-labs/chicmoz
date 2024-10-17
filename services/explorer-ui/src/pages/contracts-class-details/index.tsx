80dacd237a497f38c8451bd56c5e7bdcd1b6e373
Mauro <mauro@eijsenring.com>
2024-10-17 22:18:37 +0200

    update component

--- services/explorer-ui/src/pages/contracts-class-details/index.tsx
+++ services/explorer-ui/src/pages/contracts-class-details/index.tsx
@@ -1,22 +1,30 @@
 import { useParams } from "@tanstack/react-router";
 import { type FC } from "react";
 import { KeyValueDisplay } from "~/components/info-display/key-value-display";
-import { useContractClasses, useDeployedContractInstances } from "~/hooks";
+import { useContractClasses, useContractInstance } from "~/hooks";
 import { API_URL, aztecExplorer } from "~/service/constants";
 import { getContractData } from "./util";
-import { ContractClassesAndInstancesTable } from "~/components/contracts/class-and-instance-tables";
+import { ContractInstancesTable } from "~/components/contracts/instances/table";
+import { ContractClassesTable } from "~/components/contracts/classes/table";
+import { mapContractClasses, mapContractInstances } from "../contracts/util";
 
 export const ContractClassDetails: FC = () => {
   const { id } = useParams({
     from: "/contracts/classes/$id",
   });
-  const contractClassVersions = useContractClasses(id);
-  const contractDeployedInstances = useDeployedContractInstances(id);
+  const {
+    data: classesData,
+    isLoading: isLoadingClasses,
+    error: errorClasses,
+  } = useContractClasses(id);
+  const {
+    data: instancesData,
+    isLoading: isLoadingInstances,
+    error: errorInstances,
+  } = useContractInstance(id);
 
   if (!id) return <div>No classId</div>;
-  if (contractClassVersions.isLoading) return <div>Loading...</div>;
-  if (contractClassVersions.error) return <div>Error</div>;
-  if (!contractClassVersions.data) return <div>No data</div>;
+  if (!classesData) return <div>No data</div>;
 
   const apiEndpointUrl = `${API_URL}/${aztecExplorer.getL2ContractClasses(id)}`;
 
@@ -32,18 +40,39 @@ export const ContractClassDetails: FC = () => {
           </div>
           <div className="flex flex-col gap-4 mt-8">
             <div className="bg-white rounded-lg shadow-md p-4">
-              <KeyValueDisplay
-                data={getContractData(contractClassVersions.data[0])}
-              />
+              <KeyValueDisplay data={getContractData(classesData[0])} />
             </div>
           </div>
         </div>
-        <ContractClassesAndInstancesTable
-          classesTitle="Contract Class Versions"
-          contractClassesData={contractClassVersions}
-          instancesTitle="Deployed Instances"
-          contractInstancesData={contractDeployedInstances}
-        />
+        <div className="flex flex-col gap-4 md:flex-row ">
+          <div className="bg-white w-full rounded-lg shadow-md p-4 md:w-1/2">
+            <h3>Contract classes</h3>
+            {isLoadingClasses && <p>Loading...</p>}
+            {errorClasses && (
+              <p className="text-red-500">{errorClasses.message}</p>
+            )}
+            {!classesData && <p>No data</p>}
+            {classesData && (
+              <ContractClassesTable
+                contracts={mapContractClasses(classesData)}
+              />
+            )}
+          </div>
+          <div className="bg-white w-full rounded-lg shadow-md p-4 md:w-1/2">
+            <h3>Contract instances</h3>
+            {isLoadingInstances && <p>Loading...</p>}
+            {errorInstances && (
+              <p className="text-red-500">{errorInstances.message}</p>
+            )}
+            {instancesData ? (
+              <ContractInstancesTable
+                contracts={mapContractInstances([instancesData])}
+              />
+            ) : (
+              <p>No data</p>
+            )}
+          </div>
+        </div>
       </div>
     </div>
   );
