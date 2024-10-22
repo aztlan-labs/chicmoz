ecc74481041f9b193774987450b9be622af4139c
Filip Harald <Filip.harald@gmail.com>
2024-10-22 14:13:41 +0200

    feat: view separate versions of class id (#150)

--- services/explorer-ui/src/components/contracts/instances/columns.tsx
+++ services/explorer-ui/src/components/contracts/instances/columns.tsx
@@ -3,6 +3,9 @@ import { Link } from "@tanstack/react-router";
 import { routes } from "~/routes/__root";
 import { DataTableColumnHeader } from "~/components/data-table";
 import { type ContractInstance } from "./schema";
+import { CopyableText } from "~/components/copy-text";
+import { truncateHashString } from "~/lib/create-hash-string";
+import { getClassVersionLink } from "../utils";
 
 const text = {
   address: "ADDRESS",
@@ -30,7 +33,6 @@ export const contractsTableColumns: ColumnDef<ContractInstance>[] = [
       const r = `${routes.contracts.route}/${routes.contracts.children.instances.route}/${address}`;
       const truncatedAddress = `${address.slice(0, 6)}...${address.slice(-4)}`;
       return (
-        // TODO: make text chars equally wide (and not truncate)
         <div className="text-purple-light font-mono font-bold">
           <Link to={r}>{truncatedAddress}</Link>
         </div>
@@ -48,31 +50,11 @@ export const contractsTableColumns: ColumnDef<ContractInstance>[] = [
         title={text.version}
       />
     ),
-    cell: ({ row }) => (
-      <div className="text-purple-dark">{row.getValue("version")}</div>
-    ),
-    enableSorting: true,
-    enableHiding: false,
-  },
-  {
-    accessorKey: "blockHeight",
-    header: ({ column }) => (
-      <DataTableColumnHeader
-        className="text-purple-dark text-sm "
-        column={column}
-        title={text.blockHeight}
-      />
-    ),
-    cell: ({ row }) => {
-      const blockHeight = row.getValue("blockHeight");
-      // eslint-disable-next-line @typescript-eslint/restrict-plus-operands
-      const r = routes.blocks.route + "/" + blockHeight;
-      return (
-        <div className="text-purple-light">
-          <Link to={r}>{row.getValue("blockHeight")}</Link>
-        </div>
-      );
-    },
+    cell: ({ row }) =>
+      getClassVersionLink(
+        row.getValue("contractClassId"),
+        row.getValue("version")
+      ),
     enableSorting: true,
     enableHiding: false,
   },
@@ -85,20 +67,12 @@ export const contractsTableColumns: ColumnDef<ContractInstance>[] = [
         title={text.contractClassId}
       />
     ),
-    cell: ({ row }) => {
-      const contractClassId = row.getValue("contractClassId");
-      if (typeof contractClassId !== "string") return null;
-      const r = `${routes.contracts.route}/${routes.contracts.children.classes.route}/${contractClassId}`;
-      const truncatedContractClassId = `${contractClassId.slice(
-        0,
-        6
-      )}...${contractClassId.slice(-4)}`;
-      return (
-        <div className="text-purple-light font-mono font-bold">
-          <Link to={r}>{truncatedContractClassId}</Link>
-        </div>
-      );
-    },
+    cell: ({ row }) => (
+      <CopyableText
+        toCopy={row.getValue("contractClassId")}
+        text={truncateHashString(row.getValue("contractClassId"))}
+      />
+    ),
     enableSorting: false,
     enableHiding: false,
   },
@@ -112,7 +86,10 @@ export const contractsTableColumns: ColumnDef<ContractInstance>[] = [
       />
     ),
     cell: ({ row }) => (
-      <div className="text-purple-dark font-mono">{row.getValue("publicKeysHash")}</div>
+      <CopyableText
+        toCopy={row.getValue("publicKeysHash")}
+        text={truncateHashString(row.getValue("publicKeysHash"))}
+      />
     ),
     enableSorting: false,
     enableHiding: false,
@@ -127,7 +104,10 @@ export const contractsTableColumns: ColumnDef<ContractInstance>[] = [
       />
     ),
     cell: ({ row }) => (
-      <div className="text-purple-dark font-mono">{row.getValue("deployer")}</div>
+      <CopyableText
+        toCopy={row.getValue("deployer")}
+        text={truncateHashString(row.getValue("deployer"))}
+      />
     ),
     enableSorting: false,
     enableHiding: false,
@@ -145,7 +125,9 @@ export const contractsTableColumns: ColumnDef<ContractInstance>[] = [
       const blockHash = row.getValue("blockHash");
       if (typeof blockHash !== "string") return null;
       const r = `${routes.blocks.route}/${blockHash}`;
-      const truncatedBlockHash = `${blockHash.slice(0, 6)}...${blockHash.slice(-4)}`;
+      const truncatedBlockHash = `${blockHash.slice(0, 6)}...${blockHash.slice(
+        -4
+      )}`;
       return (
         <div className="text-purple-light font-mono font-bold">
           <Link to={r}>{truncatedBlockHash}</Link>
---
+++ services/explorer-ui/src/components/contracts/utils.tsx
@@ -0,0 +1,17 @@
+import { Link } from "@tanstack/react-router";
+import { routes } from "~/routes/__root";
+
+export const getClassVersionLink = (
+  contractClassId: string | null,
+  version: number | null,
+  linkText?: string
+) => {
+  if (typeof contractClassId !== "string") return null;
+  if (typeof version !== "number") return null;
+  const r = `${routes.contracts.route}/${routes.contracts.children.classes.route}/${contractClassId}/versions/${version}`;
+  return (
+    <div className="text-purple-light font-mono font-bold">
+      <Link to={r}>{linkText ?? version}</Link>
+    </div>
+  );
+};
