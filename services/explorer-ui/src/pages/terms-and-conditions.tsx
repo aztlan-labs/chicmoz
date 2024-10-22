c659de086de9c7b24c6e05e5a9497b896bf58977
filip <filip.harald@gmail.com>
2024-10-22 14:28:47 +0200

    toc

--- services/explorer-ui/src/pages/terms-and-conditions.tsx
+++ services/explorer-ui/src/pages/terms-and-conditions.tsx
@@ -5,7 +5,12 @@ export const TermsAndCondtions: FC = () => {
     <div className="flex flex-col items-center">
       <h1>Terms and Conditions</h1>
       <div className="bg-white w-full rounded-lg shadow-md p-4 md:w-1/2">
-        <p>TODO</p>
+        <p>
+          Chicmoz is an opensource project that aims to provide live network
+          data and a high uptime. However, Chicmoz offers no guarantees on data
+          availability or accuracy and can not be held legally accountable for
+          it.
+        </p>
       </div>
     </div>
   );
