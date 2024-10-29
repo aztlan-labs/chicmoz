dd70b52508a93b870564115b5d6eb785711f837f
Mauro <mauro@eijsenring.com>
2024-10-29 17:49:59 +0100

    update header

--- services/explorer-ui/src/components/header.tsx
+++ services/explorer-ui/src/components/header.tsx
@@ -1,7 +1,7 @@
 import { Link, useNavigate } from "@tanstack/react-router";
-import { Input, SearchInput } from "~/components/ui/input";
+import { SearchInput } from "~/components/ui/input";
 import { routes } from "~/routes/__root.tsx";
-import { Menu, Search, X } from "lucide-react";
+import { Menu, X } from "lucide-react";
 import { useEffect, useState } from "react";
 import { useSearch } from "~/hooks/search";
 import { Button } from "./ui";
@@ -12,7 +12,6 @@ export const Header = () => {
   const [searchValue, setSearchValue] = useState("");
   const [hasNoResults, setHasNoResults] = useState(false);
   const [isMenuOpen, setIsMenuOpen] = useState(false);
-  const [isSearchVisible, setIsSearchVisible] = useState(false);
 
   const navigationItems = [
     { name: routes.home.title, path: routes.home.route },
@@ -32,14 +31,18 @@ export const Header = () => {
 
       if (block) {
         void navigate({ to: `/blocks/${block.hash}` });
+        setIsMenuOpen(false);
       } else if (txEffect) {
         void navigate({ to: `/tx-effects/${txEffect.hash}` });
+        setIsMenuOpen(false);
       } else if (instance) {
         void navigate({ to: `/contracts/instances/${instance.address}` });
+        setIsMenuOpen(false);
       } else if (contractClass) {
         void navigate({
           to: `/contracts/classes/${contractClass.contractClassId}/versions/${contractClass.version}`,
         });
+        setIsMenuOpen(false);
       } else if (Object.values(data.results).every((arr) => !arr.length)) {
         setHasNoResults(true);
       }
@@ -59,17 +62,17 @@ export const Header = () => {
 
   return (
     <div className="mx-auto px-4 mt-10 max-w-[1440px] md:px-[70px]">
-      <div className="flex flex-row w-full items-center bg-purple-light rounded-[40px] py-4 pr-4 md:pl-10">
+      <div className="flex flex-col w-full items-center bg-purple-light rounded-[40px] py-4 pr-4 md:pl-10">
         <div
           className={`w-full transition-all duration-300 ease-in-out 
           ${isMenuOpen ? "rounded-b-3xl" : ""}`}
         >
           {/* Header */}
-          <div className="max-w-7xl w-full mx-auto">
+          <div className="w-full mx-auto">
             {/* Desktop Navigation */}
             <div className="hidden md:flex md:w-full md:items-center md:justify-between ">
               <ChicmozHomeLink textClasses="hidden md:block" />
-              <div className="flex  justify-center items-center">
+              <div className="flex  justify-center items-center w-1/2 sm:w-1/3 ">
                 <SearchInput
                   placeholder="Search"
                   onIconClick={handleSearch}
@@ -80,7 +83,7 @@ export const Header = () => {
                 />
               </div>
 
-              <div className="flex space-x-6 justify-center items-center">
+              <div className="flex space-x-6 justify-center items-center pr-11">
                 {navigationItems.map((item) => (
                   <Link
                     key={item.name}
@@ -94,7 +97,7 @@ export const Header = () => {
             </div>
 
             {/* Mobile Navigation Header */}
-            <div className="md:hidden flex items-center justify-between w-full px-4">
+            <div className=" flex items-center justify-between w-full px-4 md:hidden">
               <ChicmozHomeLink textClasses="block md:hidden" />
               <div className="flex items-center justify-between space-x-4">
                 <Button
@@ -122,19 +125,22 @@ export const Header = () => {
                 isMenuOpen ? "max-h-[400px] opacity-100" : "max-h-0 opacity-0"
               }`}
             >
-              <div className="px-4 py-4 space-y-6">
+              <div className="px-4 py-4 space-y-3">
                 {/* Search bar */}
-                <div className="relative">
-                  <Input
-                    type="search"
+
+                <div className="flex items-center mt-1">
+                  <SearchInput
                     placeholder="Search"
-                    className="w-full bg-white/10 border-white/20 text-white placeholder:text-white/70 h-12"
+                    onIconClick={handleSearch}
+                    onKeyDown={(e) => e.key === "Enter" && handleSearch()}
+                    onChange={(e) => handleOnChange(e.target.value)}
+                    isLoading={isLoading}
+                    noResults={hasNoResults}
                   />
-                  <Search className="absolute right-3 top-1/2 transform -translate-y-1/2 h-5 w-5 text-white/70" />
                 </div>
 
                 {/* Navigation Items */}
-                <div className="flex flex-col space-y-4">
+                <div className="flex flex-col space-y-2">
                   {navigationItems.map((item) => (
                     <Link
                       key={item.name}
@@ -148,21 +154,6 @@ export const Header = () => {
                 </div>
               </div>
             </div>
-          </div>
-        </div>
-
-        {/* Slide-down Search Bar for Mobile */}
-        <div
-          className={`md:hidden overflow-hidden transition-all duration-300 ease-in-out
-          ${isSearchVisible ? "max-h-20 opacity-100" : "max-h-0 opacity-0"}`}
-        >
-          <div className="px-4 py-2">
-            <Input
-              type="search"
-              placeholder="Search"
-              className="w-full bg-white/10 border-white/20 text-white placeholder:text-white/70 h-12"
-              autoFocus
-            />
           </div>
         </div>
       </div>
