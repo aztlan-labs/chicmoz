a2c832774de84133b2c3296076e03e61cd859f5a
filip <filip.harald@gmail.com>
2024-10-31 15:42:52 +0100

    broadcasting unconstrained events as well

--- services/aztec-event-cannon/src/cannon/index.ts
+++ services/aztec-event-cannon/src/cannon/index.ts
@@ -11,9 +11,11 @@ import {
   waitForPXE,
   //TxStatus,
   createPXEClient,
-  getContractClassFromArtifact,
+  FunctionSelector,
+  SentTx,
   //getContractInstanceFromDeployParams,
 } from "@aztec/aztec.js";
+import { FunctionType } from "@aztec/foundation/abi";
 import { getSchnorrAccount } from "@aztec/accounts/schnorr";
 import {
   // AztecAddress,
@@ -23,7 +25,10 @@ import {
 import { getInitialTestAccountsWallets } from "@aztec/accounts/testing";
 import { AZTEC_RPC_URL } from "../environment.js";
 import { logger } from "../logger.js";
-import { broadcastPrivateFunction } from "@aztec/aztec.js/deployment";
+import {
+  broadcastPrivateFunction,
+  broadcastUnconstrainedFunction,
+} from "@aztec/aztec.js/deployment";
 
 let pxe: PXE;
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
@@ -49,6 +54,26 @@ export async function init() {
   };
 }
 
+export const truncateHashString = (value: string) => {
+  const startHash = value.substring(0, 6);
+  const endHash = value.substring(value.length - 4, value.length);
+  return `${startHash}...${endHash}`;
+};
+
+async function logAndWaitForTx(tx: SentTx, additionalInfo: string) {
+  const hash = truncateHashString((await tx.getTxHash()).to0xString());
+  logger.info(`📫 TX ${hash} (${additionalInfo})`);
+  const receipt = await tx.wait();
+  logger.info(`⛏  TX ${hash} block ${receipt.blockNumber}`);
+  return receipt;
+}
+
+function getFunctionSpacer(type: FunctionType) {
+  if (type === FunctionType.PRIVATE) return type + "       ";
+  if (type === FunctionType.UNCONSTRAINED) return type + " ";
+  return type + "        ";
+}
+
 export async function start() {
   logger.info("Starting Cannon...");
   const secretKey = Fr.random();
@@ -63,26 +88,57 @@ export async function start() {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const { address, publicKeys, partialAddress } =
     schnorrAccount.getCompleteAddress();
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const tx = await schnorrAccount.deploy().wait();
-  logger.info(`Blocknumber: ${tx.blockNumber}`);
+  await logAndWaitForTx(schnorrAccount.deploy(), "Deploying account");
   const wallet = await schnorrAccount.getWallet();
-  const votingContract = await EasyPrivateVotingContract.deploy(wallet, address)
-    .send()
-    .deployed();
+  const votingContractTx = EasyPrivateVotingContract.deploy(
+    wallet,
+    address
+  ).send();
+  logger.info(
+    `📫 TX ${truncateHashString(
+      (await votingContractTx.getTxHash()).to0xString()
+    )} (Deploying voting contract)`
+  );
+  const votingContract = await votingContractTx.deployed();
   const addressString = votingContract.address.toString();
-  logger.info(`Voting Contract deployed at: ${addressString}`);
+  logger.info(`⛏  Voting Contract deployed at: ${addressString}`);
 
-  const artifact = EasyPrivateVotingContract.artifact;
-  const contractClass = getContractClassFromArtifact(artifact);
-
-  for (const fn of contractClass.privateFunctions) {
-    logger.info(`Broadcasting private function: ${fn.selector.toString()}`);
-    const tx2 = await (
-      await broadcastPrivateFunction(wallet, artifact, fn.selector)
-    )
-      .send()
-      .wait();
-    logger.info(`Blocknumber: ${tx2.blockNumber}`);
+  for (const fn of votingContract.artifact.functions) {
+    logger.info(
+      `${getFunctionSpacer(fn.functionType)}${fn.name}`
+    );
+    if (fn.functionType === FunctionType.PRIVATE) {
+      const selector = FunctionSelector.fromNameAndParameters(
+        fn.name,
+        fn.parameters
+      );
+      await logAndWaitForTx(
+        (
+          await broadcastPrivateFunction(
+            wallet,
+            votingContract.artifact,
+            selector
+          )
+        ).send(),
+        `Broadcasting private function ${fn.name}`
+      );
+    }
+    if (fn.functionType === FunctionType.UNCONSTRAINED) {
+      const selector = FunctionSelector.fromNameAndParameters(
+        fn.name,
+        fn.parameters
+      );
+      await logAndWaitForTx(
+        (
+          await broadcastUnconstrainedFunction(
+            wallet,
+            votingContract.artifact,
+            selector
+          )
+        ).send(),
+        `Broadcasting unconstrained function ${fn.name}`
+      );
+    }
   }
+  //const tx4 = await (await broadcastUnconstrainedFunction(wallet, artifact, selector)).send().wait();
 }
