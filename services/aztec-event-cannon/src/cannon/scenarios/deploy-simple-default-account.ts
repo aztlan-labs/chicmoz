7d712679f59852ebd705ff36d14669fa6bb9bff5
filip <filip.harald@gmail.com>
2024-11-18 16:10:57 +0100

    oooeps

--- services/aztec-event-cannon/src/cannon/scenarios/deploy-simple-default-account.ts
+++ services/aztec-event-cannon/src/cannon/scenarios/deploy-simple-default-account.ts
@@ -4,7 +4,7 @@ import { getPxe } from "../pxe.js";
 import { getNewAccount } from "./utils/index.js";
 
 export async function run() {
-  logger.info("VOTING CONTRACT - deploy & broadcast functions");
+  logger.info("SIMPLE DEFAULT ACCOUNT");
   const pxe = getPxe();
   await waitForPXE(pxe);
   await getNewAccount( pxe );
