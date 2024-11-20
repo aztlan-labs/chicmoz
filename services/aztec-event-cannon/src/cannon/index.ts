14bb4922469268c1e38b7ded7564f0d580849e20
filip <filip.harald@gmail.com>
2024-11-20 16:04:38 +0100

    testing and cleaning

--- services/aztec-event-cannon/src/cannon/index.ts
+++ services/aztec-event-cannon/src/cannon/index.ts
@@ -3,6 +3,7 @@ import { setup } from "./pxe.js";
 import { run as deployBroadcastFunctionsVote } from "./scenarios/deploy-broadcast-functions-vote.js";
 import { run as deploySimpleDefaultAccount } from "./scenarios/deploy-simple-default-account.js";
 import { run as deployAndInteractTokenContract } from "./scenarios/deploy-and-interact-token-contract.js";
+import { run as deploySimpleLog } from "./scenarios/deploy-and-run-simple-log.js";
 
 export async function init() {
   logger.info("Initializing Cannon...");
@@ -19,9 +20,10 @@ export async function init() {
 export const start = async () => {
   logger.info("Starting Cannon...");
   const scenarios = [
-    deploySimpleDefaultAccount,
-    deployAndInteractTokenContract,
-    deployBroadcastFunctionsVote,
+    //deploySimpleDefaultAccount,
+    //deployAndInteractTokenContract,
+    //deployBroadcastFunctionsVote,
+    deploySimpleLog,
   ] as (() => Promise<void>)[];
   for (const fn of scenarios) await fn();
 };
