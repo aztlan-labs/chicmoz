42b87fae335ff93b9bd8e2f6b2b6816b2ea4bf11
filip <filip.harald@gmail.com>
2024-11-22 11:21:51 +0100

    chore: rename aztec-event-cannon to event-cannon

--- services/aztec-event-cannon/src/environment.ts
+++
@@ -1,3 +0,0 @@
-export const NODE_ENV = process.env.NODE_ENV ?? "development";
-export const SERVICE_NAME = process.env.SERVICE_NAME ?? "aztec-event-cannon";
-export const AZTEC_RPC_URL = process.env.AZTEC_RPC_URL ?? "http://aztec-sandbox-node.localhost";
---
+++ services/event-cannon/src/environment.ts
@@ -0,0 +1,3 @@
+export const NODE_ENV = process.env.NODE_ENV ?? "development";
+export const SERVICE_NAME = process.env.SERVICE_NAME ?? "event-cannon";
+export const AZTEC_RPC_URL = process.env.AZTEC_RPC_URL ?? "http://aztec-sandbox-node.localhost";
