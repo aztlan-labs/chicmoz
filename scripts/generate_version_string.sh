379c1670bdb9e832ec45be9137158d3ad058f9f9
filip <filip.harald@gmail.com>
2024-12-13 14:09:11 +0100

    hotfix: using proper git-command for versioning

--- scripts/generate_version_string.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-GIT_TAG=$(git describe --tags --abbrev=0)
-GIT_COMMIT=$(git rev-parse --short HEAD)
-if git diff --quiet; then
-    UNCOMMITTED_CHANGES=""
-else
-    UNCOMMITTED_CHANGES="-DIRTY"
-fi
-echo "${GIT_TAG}-${GIT_COMMIT}${UNCOMMITTED_CHANGES}"
--- scripts/production/deploy.sh
+++ scripts/production/deploy.sh
@@ -2,4 +2,4 @@
 
 set -e
 
-VERSION_STRING="$(scripts/generate_version_string.sh)" skaffold run --filename "k8s/production/skaffold.production.light.yaml" --default-repo=registry.digitalocean.com/aztlan-containers
+VERSION_STRING="$(git describe --tags)" skaffold run --filename "k8s/production/skaffold.production.light.yaml" --default-repo=registry.digitalocean.com/aztlan-containers
