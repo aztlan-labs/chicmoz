1dad9f9a249a7e81a61d81a65051702dd50170f5
filip <filip.harald@gmail.com>
2024-12-13 12:51:34 +0100

    hotfix: missed quote in script

--- scripts/production/deploy.sh
+++ scripts/production/deploy.sh
@@ -2,4 +2,4 @@
 
 set -e
 
-VERSION_STRING="$(scripts/set_version_string.sh) skaffold run --filename "k8s/production/skaffold.production.light.yaml" --default-repo=registry.digitalocean.com/aztlan-containers
+VERSION_STRING="$(scripts/set_version_string.sh)" skaffold run --filename "k8s/production/skaffold.production.light.yaml" --default-repo=registry.digitalocean.com/aztlan-containers
