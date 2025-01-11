0c42973eacf31ad34f181c1c321fa7fd92822087
filip <filip.harald@gmail.com>
2025-01-11 15:44:24 +0100

    chore: ethereum-listener for production

--- services/ethereum-listener/src/environment.ts
+++ services/ethereum-listener/src/environment.ts
@@ -9,8 +9,8 @@ export const ETHEREUM_HTTP_RPC_URL = process.env.ETHEREUM_HTTP_RPC_URL ?? "http:
 export const ETHEREUM_WS_RPC_URL = process.env.ETHEREUM_WS_RPC_URL ?? "ws://anvil-ethereum-node:8545";
 export const AZTEC_CHAIN_NAME = process.env.AZTEC_CHAIN_NAME ?? "AZTEC";
 export const AZTEC_NETWORK_NAME = process.env.AZTEC_NETWORK_NAME ?? "SANDBOX";
-export const ETHEREUM_CHAIN_NAME = process.env.ETHEREUM_CHAIN_NAME ?? "LOCAL_ETHEREUM";
-export const ETHEREUM_NETWORK_ID = Number(process.env.ETHEREUM_NETWORK_NAME) || 31337;
+export const ETHEREUM_CHAIN_NAME = process.env.ETHEREUM_CHAIN_NAME ?? "HARDCODED_ETHEREUM";
+export const ETHEREUM_NETWORK_ID = Number(process.env.ETHEREUM_NETWORK_NAME) || 99999;
 
 export const KAFKA_CONNECTION = process.env.KAFKA_CONNECTION ?? "kafka:9092";
 export const KAFKA_SASL_USERNAME = process.env.KAFKA_SASL_USERNAME ?? "controller_user";
