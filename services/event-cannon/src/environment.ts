381e703793053a4c0dedc854e79ed4e78aa76db6
Filip Harald <Filip.harald@gmail.com>
2025-01-20 13:23:15 +0100

    fix: auth-service not working (#271)

--- services/event-cannon/src/environment.ts
+++ services/event-cannon/src/environment.ts
@@ -1,6 +1,6 @@
 export const NODE_ENV = process.env.NODE_ENV ?? "development";
 export const SERVICE_NAME = process.env.SERVICE_NAME ?? "event-cannon";
 
-export const AZTEC_RPC_URL = process.env.AZTEC_RPC_URL ?? "http://aztec-sandbox-node.localhost";
-export const ETHEREUM_RPC_URL = process.env.ETHEREUM_RPC_URL ?? "http://anvil-ethereum-node.localhost";
-export const EXPLORER_API_URL = process.env.EXPLORER_API_URL ?? "http://explorer-api.localhost/v1/d1e2083a-660c-4314-a6f2-1d42f4b944f4";
+export const AZTEC_RPC_URL = process.env.AZTEC_RPC_URL ?? "http://aztec.sandbox.chicmoz.localhost";
+export const ETHEREUM_RPC_URL = process.env.ETHEREUM_RPC_URL ?? "http://eth.sandbox.chicmoz.localhost";
+export const EXPLORER_API_URL = process.env.EXPLORER_API_URL ?? "http://api.sandbox.chicmoz.localhost/v1/d1e2083a-660c-4314-a6f2-1d42f4b944f4";
