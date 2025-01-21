0370bd5894819c2577694da5e49d683a54218dcd
filip <filip.harald@gmail.com>
2025-01-21 09:25:01 +0100

    Revert "init"
    This reverts commit babc0746b0fa3c68954831e9e5457449f32fc10c.

--- services/event-cannon/src/cannon/scenarios/simple-deploy-contract.ts
+++ services/event-cannon/src/cannon/scenarios/simple-deploy-contract.ts
@@ -1,20 +1,12 @@
-import {
-  DeploySentTx,
-  NoirCompiledContract,
-  PublicKeys,
-  getContractInstanceFromDeployParams,
-  loadContractArtifact,
-  waitForPXE,
-} from "@aztec/aztec.js";
-import { EasyPrivateVotingContract } from "@aztec/noir-contracts.js/EasyPrivateVoting";
-import * as contractArtifactJson from "@aztec/noir-contracts.js/artifacts/easy_private_voting_contract-EasyPrivateVoting" assert { type: "json" };
-import assert from "assert";
+import { DeploySentTx, waitForPXE } from "@aztec/aztec.js";
 import { logger } from "../../logger.js";
 import { getAztecNodeClient, getPxe, getWallets } from "../pxe.js";
 import {
   deployContract,
   registerContractClassArtifact,
 } from "./utils/index.js";
+import { EasyPrivateVotingContract } from "@aztec/noir-contracts.js/EasyPrivateVoting";
+import * as contractArtifactJson from "@aztec/noir-contracts.js/artifacts/easy_private_voting_contract-EasyPrivateVoting" assert { type: "json" };
 
 export async function run() {
   logger.info("===== SIMPLE DEPLOY CONTRACT =====");
@@ -37,35 +29,6 @@ export async function run() {
     deployFn: (): DeploySentTx<EasyPrivateVotingContract> => sentTx,
     node: getAztecNodeClient(),
   });
-
-  // validation logic
-  const artifact = loadContractArtifact(
-    (contractArtifactJson as unknown as { default: NoirCompiledContract }).default
-  );
-  const tempInst = getContractInstanceFromDeployParams(artifact, {
-    constructorArgs: [votingAdmin],
-    deployer: deployerWallet.getAddress(),
-    skipArgsDecoding: false,
-    salt: contract.instance.salt,
-    publicKeys: contract.instance.publicKeys,
-  });
-  if (tempInst === undefined) throw new Error("Initialization hash mismatch");
-
-  assert(tempInst.contractClassId.equals(contract.instance.contractClassId));
-  assert(
-    tempInst.initializationHash.equals(contract.instance.initializationHash)
-  );
-  assert(tempInst.salt.equals(contract.instance.salt));
-  assert(tempInst.deployer.equals(contract.instance.deployer));
-  assert(tempInst.version === contract.instance.version);
-  assert(tempInst.publicKeys.equals(contract.instance.publicKeys));
-
-  const buf = contract.instance.publicKeys.toBuffer();
-  const str = buf.toString("hex");
-  const buf2 = Buffer.from(str, "hex");
-  assert(PublicKeys.fromBuffer(buf2).equals(contract.instance.publicKeys));
-
-  // validation logic end
 
   registerContractClassArtifact(
     contractLoggingName,
