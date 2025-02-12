e9baf032ff982ae38c791ebe5861525c88a2aac9
filip <filip.harald@gmail.com>
2025-02-12 11:14:45 +0100

    test1

--- scripts/production/deploy.sh
+++ scripts/production/deploy.sh
@@ -2,4 +2,4 @@
 
 set -e
 
-VERSION_STRING="$(git describe --tags)" skaffold run --filename "k8s/production/skaffold.full.yaml" --default-repo=registry.digitalocean.com/aztlan-containers
+VERSION_STRING="$(git describe --tags)" skaffold run --filename "k8s/production/skaffold.light.yaml" --default-repo=registry.digitalocean.com/aztlan-containers
