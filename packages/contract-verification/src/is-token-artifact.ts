743b363afde3885738d5deae597b910596ca8b3d
filip <filip.harald@gmail.com>
2025-02-13 16:57:49 +0100

    init

---
+++ packages/contract-verification/src/is-token-artifact.ts
@@ -0,0 +1,94 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+import { NoirCompiledContract } from "@aztec/aztec.js";
+import { z } from "zod";
+
+const transferToPrivateStr = "transfer_to_private";
+const transferToPublicStr = "transfer_to_public";
+const transferInPrivateStr = "transfer_in_private";
+const transferInPublicStr = "transfer_in_public";
+
+const requiredFunctionNames = [
+  transferToPrivateStr,
+  transferToPublicStr,
+  transferInPrivateStr,
+  transferInPublicStr,
+];
+
+const privateSchema = z.string().regex(/private/);
+const publicSchema = z.string().regex(/public/);
+const anyFunction = z.object({
+  name: z.string(),
+  custom_attributes: z.array(z.string()),
+});
+const transferToPrivate = z.object({
+  name: z.string().regex(new RegExp(`^${transferToPrivateStr}$`)),
+  custom_attributes: z.array(privateSchema),
+});
+const transferToPublic = z.object({
+  name: z.string().regex(new RegExp(`^${transferToPublicStr}$`)),
+  custom_attributes: z.array(privateSchema),
+});
+const transferInPrivate = z.object({
+  name: z.string().regex(new RegExp(`^${transferInPrivateStr}$`)),
+  custom_attributes: z.array(privateSchema),
+});
+const transferInPublic = z.object({
+  name: z.string().regex(new RegExp(`^${transferInPublicStr}$`)),
+  custom_attributes: z.array(publicSchema),
+});
+
+const requiredFunctions = z.union([
+  transferToPrivate,
+  transferToPublic,
+  transferInPrivate,
+  transferInPublic,
+]);
+
+const tokenSchema = z.object({
+  functions: z
+    .array(requiredFunctions.or(anyFunction))
+    .superRefine(
+      (
+        value: Array<Record<string, string | string[]>>,
+        ctx: z.RefinementCtx
+      ) => {
+        const functionNames = value.map((f) => f.name);
+        const missingFunctions = requiredFunctionNames.filter(
+          (fn) => !functionNames.includes(fn)
+        );
+        if (missingFunctions.length !== 0) {
+          ctx.addIssue({
+            code: z.ZodIssueCode.custom,
+            message: `Missing functions: ${missingFunctions.join(", ")}`,
+          });
+        }
+      }
+    ),
+});
+const customErrorMap: z.ZodErrorMap = (_error, ctx: z.ErrorMapCtx) => {
+  if (ctx?.data?.toString()?.length < 100)
+    return { message: `${ctx.defaultError} (with data: ${ctx.data})` };
+  return { message: ctx.defaultError };
+};
+
+export const isTokenArtifact = (
+  artifact: NoirCompiledContract
+): {
+  result: boolean;
+  details: string;
+} => {
+  let details = "";
+  try {
+    tokenSchema.parse(artifact, { errorMap: customErrorMap });
+  } catch (err) {
+    if (err instanceof z.ZodError)
+      details = err.errors.map((e) => JSON.stringify(e)).join("\n");
+    // eslint-disable-next-line no-console
+    else console.error(err);
+  }
+  return {
+    result: details === "",
+    details,
+  };
+};
