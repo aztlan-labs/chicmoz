822b42a6f0960e26b840c4feec8e4864425466f7
filip <filip.harald@gmail.com>
2025-02-13 14:23:53 +0100

    stuff

--- services/event-cannon/src/environment.ts
+++ services/event-cannon/src/environment.ts
@@ -12,30 +12,20 @@ export const EXPLORER_API_URL =
 export const SCENARIO_DELAY = z.coerce
   .number()
   .default(1000)
+  .optional()
   .parse(process.env.SCENARIO_DELAY);
 
-export const SCENARIO_SIMPLE_DEFAULT_ACCOUNT = false;
-export const SCENARIO_TOKEN_CONTRACT = true;
-export const SCENARIO_FUNCTIONS_VOTE = true;
-export const SCENARIO_SIMPLE_CONTRACT = true;
-export const SCENARIO_SIMPLE_LOG = true;
-export const SCENARIO_L1L2_PUBLIC_MESSAGING = true;
-export const SCENARIO_L1L2_PRIVATE_MESSAGING = true;
-
-
-//export const SCENARIO_SIMPLE_DEFAULT_ACCOUNT =
-//  process.env.SCENARIO_SIMPLE_DEFAULT_ACCOUNT === "true" ?? false;
-//export const SCENARIO_TOKEN_CONTRACT =
-//  process.env.SCENARIO_TOKEN_CONTRACT === "true" ?? false;
-//export const SCENARIO_FUNCTIONS_VOTE =
-//  process.env.SCENARIO_FUNCTIONS_VOTE === "true" ?? false;
-//export const SCENARIO_SIMPLE_CONTRACT =
-//  process.env.SCENARIO_SIMPLE_CONTRACT === "true" ?? false;
-//export const SCENARIO_SIMPLE_LOG =
-//  process.env.SCENARIO_SIMPLE_LOG === "true" ?? false;
-//export const SCENARIO_L1L2_PUBLIC_MESSAGING =
-//  process.env.SCENARIO_L1L2_PUBLIC_MESSAGING === "true" ?? false;
-//export const SCENARIO_L1L2_PRIVATE_MESSAGING =
-//  process.env.SCENARIO_L1L2_PRIVATE_MESSAGING === "true" ?? false;
-
-// yarn build && NODE_ENV=production AZTEC_RPC_URL=http://34.83.184.137:8080 ETHEREUM_RPC_URL=http://34.83.148.196:8545 EXPLORER_API_URL=http://api.devnet.aztecscan.xyz/v1/temporary-api-key SCENARIO_DELAY=10000 SCENARIO_SIMPLE_CONTRACT=true yarn start
+export const SCENARIO_SIMPLE_DEFAULT_ACCOUNT =
+  process.env.SCENARIO_SIMPLE_DEFAULT_ACCOUNT === "true" ?? false;
+export const SCENARIO_TOKEN_CONTRACT =
+  process.env.SCENARIO_TOKEN_CONTRACT === "true" ?? false;
+export const SCENARIO_FUNCTIONS_VOTE =
+  process.env.SCENARIO_FUNCTIONS_VOTE === "true" ?? false;
+export const SCENARIO_SIMPLE_CONTRACT =
+  process.env.SCENARIO_SIMPLE_CONTRACT === "true" ?? false;
+export const SCENARIO_SIMPLE_LOG =
+  process.env.SCENARIO_SIMPLE_LOG === "true" ?? false;
+export const SCENARIO_L1L2_PUBLIC_MESSAGING =
+  process.env.SCENARIO_L1L2_PUBLIC_MESSAGING === "true" ?? false;
+export const SCENARIO_L1L2_PRIVATE_MESSAGING =
+  process.env.SCENARIO_L1L2_PRIVATE_MESSAGING === "true" ?? false;
