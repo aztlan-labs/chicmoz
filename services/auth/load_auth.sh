76e65526629cad48a7fce9375f0608b4f214798d
Filip Harald <Filip.harald@gmail.com>
2025-02-28 08:12:04 +0100

    chore: workflow to build on PRs (#337)

--- services/auth/load_auth.sh
+++
@@ -1,5 +0,0 @@
-# tests rate limiting by triggering requests to the dummy node
-# TODO: delete once we have integration tests
-for i in $(seq 1 51); do
-    curl node.localhost/v1/a8c9e08c-0f74-49d4-a96d-249b563f3f7a/coso
-done
